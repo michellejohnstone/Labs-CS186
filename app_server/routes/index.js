25e7b0399e48a527ad4bb5a2cb065a68b64ff4c1
Michelle Johnstone <mjohnstone@mills.edu>
2017-02-14 22:15:36 +0000

    finished changes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
+var labsix = require('../controllers/labsix');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+router.get('/html1', labsix.index);
 
 module.exports = router;
